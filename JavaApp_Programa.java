acfb25e5aabb722899c8d1fe2016775e08900a0c
germanrcb110566 <52899431+germanrcb110566@users.noreply.github.com>
2019-07-14 22:05:42 -0500

    Versión con Opciones de Suma y Multiplicación
    Versión  fuente con opciones de suma y Multiplicación

--- JavaApp_Programa.java
+++ JavaApp_Programa.java
@@ -22,13 +22,29 @@ public class JavaApp_Programa {
         //en pantalla.
         
         Scanner dato= new Scanner(System.in);
-        double numero1, numero2, suma;
+        double numero1, numero2, resultado;
+        String operacion="";
+        
+        System.out.println("Desea Sumar o Multiplicar:");       
+        while ( !"Sumar".equals(operacion) && !"Multiplicar".equals(operacion) )
+        {
+            System.out.println("Ingrese Sumar o Multiplicar");
+            operacion=dato.next();
+        }
+        
         System.out.println("Ingrese primer Numero:");
         numero1=dato.nextDouble();
         System.out.println("Ingrese Segundo Numero:");
         numero2=dato.nextDouble();
-        suma=numero1+numero2;
-        System.out.println("La suma de los dos numeros es:" + suma);
+        if ("Sumar".equals(operacion)) 
+        {            
+            resultado=numero1+numero2;
+        } 
+        else 
+        {
+            resultado=numero1*numero2;
+        }
+        System.out.println("El resultado de la operacion de los dos numeros es:" + resultado);
         
     }
     
